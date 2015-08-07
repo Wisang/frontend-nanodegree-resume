617a8ea14d228764aebc3c3937e44c22a88c411b
Wisang <wseom7@gmail.com>
2015-08-07 16:22:52 +0900

    shows work experience

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,16 +1,13 @@
-//$("#main").append(["Wisang Eom"]);
-//var awesomeThought = "I am Wisang and I am AWESOME";
-//var funThought = awesomeThought.replace("AWESOME", "FUN");
 
-// var name = "Wisang Eom";
-// var role = "Manger";
-
-// var formattedName = HTMLheaderName.replace("%data%", name);
-// var formattedRole = HTMLheaderRole.replace("%data%", role);
 var bio = {
 	"name": "Wisang Eom",
-	"role": "Manager",
-	"contacts": "wseom7@gmail.com",
+	"role": "Stupid Manager, Pseudo Developer",
+	"contacts": {
+		"email": "wseom7@gmail.com", 
+		"twitter": "@wseom", 
+		"mobile": "010-2330-9854",
+		"github": "Wisang"
+	},
 	"skills": ["ppt", "presenting", "lecturing", "some C", "some Java"],
 	"picture": "https://pbs.twimg.com/profile_images/2169417634/Wisang.jpg",
 	"welcome": "Welcome to my Bio"	
@@ -42,25 +39,38 @@ var work ={
 	{
 		"employer": "LG Electronics",
 		"position": "Research Fellow",
-		"years": "2015 ~ ",
-		"city": "seoul"
+		"dates": "2015 ~ future",
+		"location": "seoul",
+		"description": "has been working as Agile Team Leader"
 	},
 	{
 		"employer": "LG Electronics",
 		"position": "Principal Research Engineer",
-		"years": "2012 ~ 2014 ",
-		"city": "seoul"
+		"dates": "2012 ~ 2014 ",
+		"location": "seoul",
+		"description": "has been working as Agile Team Leader and Agile Coach & Instructor"
 	}
 	]
 }
 
 var project = {};
 
-// $("#header").prepend(formattedRole);
-// $("#header").prepend(formattedName);
-$("#main").append(bio.contacts);
-$("#main").append(work["position"]);
-$("#main").append(education.schools[1].name);
+var formattedName = HTMLheaderName.replace("%data%", bio.name);	
+$("#header").append(formattedName);
+
+var formattedRole= HTMLheaderRole.replace("%data%", bio.role);	
+$("#header").append(formattedRole);
+
+var formattedMobile = HTMLmobile.replace("%data%", bio.contacts["mobile"]);
+var formattedEMail = HTMLemail.replace("%data%", bio.contacts["email"]);
+var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts["twitter"]);
+var formattedGithub = HTMLgithub.replace("%data%", bio.contacts["github"]);
+
+$("#header").append(formattedMobile);
+$("#header").append(formattedEMail);
+$("#header").append(formattedTwitter);
+$("#header").append(formattedGithub);
+
 
 if(bio.skills) {
 	$("#header").append(HTMLskillsStart);
@@ -72,11 +82,36 @@ if(bio.skills) {
 	}
 }
 
-for(job in work.jobs) {
-	$("#workExperience").append(HTMLworkStart);
-	var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-	var formattedJobTitle = HTMLworkTitle.replace("%data%", work.jobs[job].position);
-	$(".work-entry:last").append(formattedEmployer).append(formattedJobTitle);
+displayWork();
+$("#header").append(HTMLprojectStart);
+
+$('#main').append(internationalizeButton);
+
+function inName(name) {
+	name = name.trim().split(" ");
+	name[1] = name[1].toUpperCase();
+	name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+
+	return name[0] + " " + name[1];
+}
+
+function displayWork() {
+	for(job in work.jobs) {
+		$("#workExperience").append(HTMLworkStart);
+		var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+		var formattedJobTitle = HTMLworkTitle.replace("%data%", work.jobs[job].position);
+		var formattedEmployerTitle = formattedEmployer + formattedJobTitle;
+		$(".work-entry:last").append(formattedEmployerTitle);
+
+		// var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+		// $(".work-entry:last").append(formattedLocation);
+
+		var formattedWorkDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+		$(".work-entry:last").append(formattedWorkDates);
+		
+		var formattedWorkDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+		$(".work-entry:last").append(formattedWorkDescription);
+	}
 }
 
 
